409a19236bdd3c8c0188b6d6cc9a31b7f8f970c7
Your Name <you@example.com>
2024-06-07 02:14:19 +0700

    login

--- app/index.tsx
+++ app/index.tsx
@@ -5,16 +5,30 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { router } from 'expo-router';
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function Index() {
+  const [username,setusername]=useState()
+  const [password,setpassword]=useState()
+  const login=async ()=>{
+   try{
+  const response=await axios.post('http://192.168.1.38:8000' + '/api/login',{username,password})
+
+  router.push('home')
+   } catch (error){
+    console.log(error)
+   }
+   
+  }
   return (
     <SafeAreaView style={{ minHeight: '100%', justifyContent: 'center',  backgroundColor: 'white' }}>
       <View style={{padding:50, gap:16}}>
       <Text style={{fontSize:42,fontWeight:'bold',textAlign:"center" }}>BSI.NET</Text>
       <Text style={{ fontWeight: 'bold', fontSize: 24, marginBottom: 24, paddingTop:150, }}>Login</Text>
-      <TextInput placeholder='Username' placeholderTextColor={'grey'} style={{ borderWidth: 1, borderRadius: 16, borderColor: '#ddd', paddingVertical: 8, paddingHorizontal: 16 }} />
-      <TextInput placeholder='Password' placeholderTextColor={'grey'} style={{ borderWidth: 1, borderRadius: 16, borderColor: '#ddd', paddingVertical: 8, paddingHorizontal: 16, marginBottom: 24 }} />
-      <Pressable style={{backgroundColor:"blue",padding:13,borderRadius:16}} onPress={() => router.push('home')}  >
+      <TextInput value={username} onChangeText={setusername} placeholder='Username' placeholderTextColor={'grey'} style={{ borderWidth: 1, borderRadius: 16, borderColor: '#ddd', paddingVertical: 8, paddingHorizontal: 16 }} />
+      <TextInput value={password} onChangeText={setpassword} placeholder='Password' placeholderTextColor={'grey'} style={{ borderWidth: 1, borderRadius: 16, borderColor: '#ddd', paddingVertical: 8, paddingHorizontal: 16, marginBottom: 24 }} />
+      <Pressable style={{backgroundColor:"blue",padding:13,borderRadius:16}} onPress={login}  >
         <Text style={{color:"white",textAlign:"center",fontWeight:"bold",}}>Login</Text>
       </Pressable>
       </View>
