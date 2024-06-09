c591e62a0b26c7b6ccca7a76de8764ca38cdc969
Your Name <you@example.com>
2024-06-09 18:33:39 +0700

    push

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Platform, SafeAreaView, Text, TextInput, Button,Pressable, View } from 'react-native';
+import { Image, StyleSheet, Platform, SafeAreaView, Text, TextInput, Button,Pressable, View,} from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -26,9 +26,9 @@ export default function Index() {
       <View style={{padding:50, gap:16}}>
       <Text style={{fontSize:42,fontWeight:'bold',textAlign:"center" }}>BSI.NET</Text>
       <Text style={{ fontWeight: 'bold', fontSize: 24, marginBottom: 24, paddingTop:150, }}>Login</Text>
-      <TextInput value={username} onChangeText={setusername} placeholder='Username' placeholderTextColor={'grey'} style={{ borderWidth: 1, borderRadius: 16, borderColor: '#ddd', paddingVertical: 8, paddingHorizontal: 16 }} />
-      <TextInput value={password} onChangeText={setpassword} placeholder='Password' placeholderTextColor={'grey'} style={{ borderWidth: 1, borderRadius: 16, borderColor: '#ddd', paddingVertical: 8, paddingHorizontal: 16, marginBottom: 24 }} />
-      <Pressable style={{backgroundColor:"blue",padding:13,borderRadius:16}} onPress={login}  >
+      <TextInput placeholder='Username' placeholderTextColor={'grey'} style={{ borderWidth: 1, borderRadius: 16, borderColor: '#ddd', paddingVertical: 8, paddingHorizontal: 16 }} />
+      <TextInput placeholder='Password' placeholderTextColor={'grey'} style={{ borderWidth: 1, borderRadius: 16, borderColor: '#ddd', paddingVertical: 8, paddingHorizontal: 16, marginBottom: 24 }} />
+      <Pressable style={{backgroundColor:"blue",padding:13,borderRadius:16}}onPress={() => router.push('home')}   >
         <Text style={{color:"white",textAlign:"center",fontWeight:"bold",}}>Login</Text>
       </Pressable>
       </View>
